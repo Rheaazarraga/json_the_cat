69fc539da5e5232c0a8943076e12a7af6e844767
Rheaazarraga <rheaazarraga@gmail.com>
2021-08-24 04:44:28 +0000

    Refactor code using callbacks and removing console.log statements. Implement fetchBreedDescription function and module. export it for the index file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,21 @@
-const request = require('request'); // use request library to make http request
-const breedName = process.argv[2]; //breed name entered by user
-//http request
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (error) {
-    console.log('error: ', error);
-    } 
-    const data = JSON.parse(body);//converts the JSON string into an object
-    if (!data[0]) {
-        console.log(`\n the ${breedName} was not found`);
-        return
+const request = require('request');
+
+const fetchBreedDescription = (breedName, callback) => {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback('error found: ', null);
+      return;
+    }
+    const breed = JSON.parse(body);
+    if (!breed[0]) {
+      callback(`The breed ${breedName} was not found`, null);
+      return;
     }
     if (response.statusCode === 200) {
-      console.log(`description: ${data[0].description}`);
+      callback(null, breed[0].description);
     }
   });
+};
+
+module.exports = { fetchBreedDescription };
